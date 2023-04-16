635d817a999ff92f9fad3200139fcc6fb97429c1
Alexey <102233166+KozyrevAlexey@users.noreply.github.com>
2023-04-16 18:18:19 +0300

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <span className="footer__span"> &#169; 2020 Mesto Russia</span>
-    </footer>
-  );
-}
